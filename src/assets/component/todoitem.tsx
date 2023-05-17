f876193c039b2631e18e3156feb4cc2be725f6af
Alanhis <ala1234765@gmail.com>
2023-05-17 11:58:27 +0300

    Удаление не нужных частей

--- src/assets/component/todoitem.tsx
+++ src/assets/component/todoitem.tsx
@@ -1,27 +1,19 @@
-import { useState } from 'react';
-import { Props } from 'react-modal';
 import { onEnterPress, useStore } from '../../service/store';
 import '../../App.css';
 export const TodoItem = ({ todo }: any) => {
   const todoList = useStore();
-  const [newText, setText] = useState('');
-  const [isEditing, setEdit] = useState(false);
-
-  const saveText = () => {
-    todo.updateText(newText);
-    setEdit(false);
-    setText('');
-  };
 
   return (
     <div className="todo-item">
       <div className="todo_info">
-        <span>{todo.text}</span>
-        <input
-          type="checkbox"
-          onChange={todo.toggleIsDone}
-          defaultChecked={todo.isDone}
-        ></input>
+        <div>
+          <span>{todo.text}</span>
+          <input
+            type="checkbox"
+            onChange={todo.toggleIsDone}
+            defaultChecked={todo.isDone}
+          ></input>
+        </div>
 
         <button onClick={() => todoList.removeTodo(todo)}>X</button>
       </div>
